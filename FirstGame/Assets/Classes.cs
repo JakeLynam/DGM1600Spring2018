7e8f185ca5d73e02e5a58c62ef5a2be74ca4b4d3
Robert Jacob Lynam <rjacoblynam@gmail.com>
2018-01-16 11:18:29 -0700

    Added classes with comments

---
+++ FirstGame/Assets/Classes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class is an object classification
+//must have open and close curly braces per object 
+public class Classes : MonoBehaviour {
+
+//void is a function for empty or with no return
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
