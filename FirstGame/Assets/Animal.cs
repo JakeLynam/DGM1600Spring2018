b43192a717099e80883c26adc13cabf447d45a57
Robert Jacob Lynam <rjacoblynam@gmail.com>
2018-01-23 13:29:49 -0700

    Created the Conventions and Syntax assignment

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	
+	void Start () 
+	{
+		print ("I love my cat";)
+		print ("I actually don't have a cat");
+		
+	}
+	
+	
+	void OnMouseDown() 
+	{
+		print ("meow.");
+		
+	}
+
+
+
+
+
+
+}
---
+++ FirstGame/Assets/Conventions and Syntax.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void OnMouseDown () 
+	{
+		print ("Wa-hoo");
+		print ("Mario has jumped")
+		
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			Debug.Log
+	}
+
+	Void OnMouseDown()
+
+	{
+		print("Do a little dance");
+	}
+
+	void OnMouseUP()
+	{
+		prin ("Why you mouse up?");
+	}
+
+	Void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+		Debug.Log("F key was pressed");
+
+		if (Input.GetKeyUp(KeyCode.F))
+		Debug.Log("F key was released");
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
