754e55f93cdc2c4d26070997470ff636a04988f2
Robert Jacob Lynam <rjacoblynam@gmail.com>
2018-01-25 14:15:20 -0700

    Turned in Variables Assignment

--- FirstGame/Assets/Variables.cs
+++ FirstGame/Assets/Variables.cs
@@ -2,15 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Variables : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+/*
+Variables are storage locations with values that can be edited
+These things can be added to game objects like characters models, 3d objects, ect.
+Here, I'm going to make a Public string, it's public so it can be accessed by other codes and can be seen in Unity's inspector
+*/
+	string Mystring Jake;
+	int myInt = 5;
+	string Character1Name = Steve;
+	int LifeTotal = 100;
+	int ArmorTotal = 100;
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+// this variable is a place where I have put my name, but it has my name because it's grabbing the info from a string
+//Thus, the answer for "My name is..." can change according to what string you want to call to
+
+	void Start () 
+	{
+		Debug.log("My name is" + Jake);
 	}
+//Some types of variables are local variables, parmameters, fields, or array elements. 	
+	
+	Void Update ()
+	{
+		Debug.Log (myInt * 2);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
