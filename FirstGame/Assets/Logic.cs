138c6061eebdc3b25fd3066b8dfc10e0679f6a45
Robert Jacob Lynam <rjacoblynam@gmail.com>
2018-02-06 13:09:40 -0700

    Created logic assignment, still need to finish

---
+++ FirstGame/Assets/Logic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour {
+
+public string password = "love";
+public int a;
+public int b;
+
+	
+	void Start () 
+	{
+		if (A > B && password == "love")
+		{
+			print("Password is correct");
+		}
+	}
+	
+	
+	void Update () {
+		
+	}
+}
